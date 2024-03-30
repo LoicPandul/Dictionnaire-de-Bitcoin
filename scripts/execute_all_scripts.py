9e1c0a3b34c24d64eba113bc148c89c1a9c3938e
Loïc Morel <137194052+LoicPandul@users.noreply.github.com>
2024-03-30 17:55:45 +0100

    Update execute_all_scripts.py

--- scripts/execute_all_scripts.py
+++ scripts/execute_all_scripts.py
@@ -5,14 +5,28 @@ chemin_dossier_scripts = '.'
 
 script_actuel = os.path.basename(__file__)
 
+scripts_prioritaires = [
+    'generate_dictionary_files.py',
+    'update_index.py',
+    'stats.py'
+]
+
 print("Début de l'exécution des scripts Python :")
 
+for script in scripts_prioritaires:
+    chemin_complet = os.path.join(chemin_dossier_scripts, script)
+    if os.path.isfile(chemin_complet):
+        print(f"Exécution de {script}...")
+        subprocess.run(['python', chemin_complet], check=True)
+        print(f"{script} exécuté avec succès.")
+    else:
+        print(f"Le fichier {script} n'a pas été trouvé et n'a pas pu être exécuté.")
+
 for fichier in os.listdir(chemin_dossier_scripts):
-    chemin_complet = os.path.join(chemin_dossier_scripts, fichier)
-    if fichier.endswith('.py') and fichier != script_actuel:
+    if fichier.endswith('.py') and fichier not in scripts_prioritaires + [script_actuel]:
+        chemin_complet = os.path.join(chemin_dossier_scripts, fichier)
         print(f"Exécution de {fichier}...")
         subprocess.run(['python', chemin_complet], check=True)
         print(f"{fichier} exécuté avec succès.")
 
 print("Tous les scripts ont été exécutés.")
-
