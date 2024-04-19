f7e2575c1c45990497ef8b3f37c244f097c3ccbe
Loïc Morel <137194052+LoicPandul@users.noreply.github.com>
2024-04-19 15:19:54 +0200

    pdf

--- scripts/PDF/pdf.py
+++ scripts/PDF/pdf.py
@@ -13,6 +13,6 @@ def convert_md_to_pdf(md_file_path, pdf_output_path):
     print(f"Le fichier PDF a été créé : {pdf_output_path}")
 
 markdown_path = "../../autres_formats/dictionnaire_complet/dictionnaire_complet.md"
-pdf_path = "../../dictionnaire_complet.pdf"
+pdf_path = "../../Dictionnaire de Bitcoin.pdf"
 
 convert_md_to_pdf(markdown_path, pdf_path)
